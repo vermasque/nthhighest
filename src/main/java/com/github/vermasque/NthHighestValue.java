6d463e607d72549431568d0b44789eb8485d1883
Sahil Verma <vermasque@gmail.com>
2018-04-01 20:18:24 -0400

    Skeleton passing when running "mvn test"

---
+++ src/main/java/com/github/vermasque/NthHighestValue.java
@@ -0,0 +1,9 @@
+package com.github.vermasque;
+
+/**
+ * A data structure that tracks the nth highest 
+ * value in an unbounded sequence of values.
+ */
+public class NthHighestValue {
+
+}
---
+++ src/test/java/com/github/vermasque/NthHighestValueTest.java
@@ -0,0 +1,12 @@
+package com.github.vermasque;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class NthHighestValueTest {
+  @Test
+  public void sequenceLargerThanN() {
+    assertTrue(true);
+  }
+}
