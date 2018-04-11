89d08075d5663a016ad6da494cad95a44b169936
Sahil Verma <vermasque@gmail.com>
2018-04-10 23:27:29 -0400

    got it to compile and pass style

--- src/main/java/com/github/vermasque/NthHighestValue.java
+++ src/main/java/com/github/vermasque/NthHighestValue.java
@@ -1,9 +1,89 @@
 package com.github.vermasque;
 
-/**
- * A data structure that tracks the nth highest 
- * value in an unbounded sequence of values.
- */
-public class NthHighestValue {
+import java.util.Optional;
 
+/**
+ * Tracks the nth-highest value in an infinite sequence of values.
+ */
+public class NthHighestValue<T extends Comparable> {
+
+  private static final int INDEX_UNDEFINED = -1;
+
+  private final Object[] maxHeap;  
+  private int heapSize = 0;
+  private int minValueIndex = INDEX_UNDEFINED;
+
+  /**
+   * Create a new NthHighestValue instance based on the value of n.
+   *
+   * @param capacity maximum count of elements stored (the n in nth-highest)
+   */
+  public NthHighestValue(final int capacity) {
+    this.maxHeap = new Object[capacity];
+  }
+
+  /** 
+   * Might return the nth-highest value if at least 
+   * n values have been encountered via calls to the 
+   * update method.  Provides this value in constant time.
+   */
+  public Optional<T> get() {
+    return isHeapFull() ? Optional.of(getMinValue()) : Optional.empty();
+  }
+
+  /**
+   * Potentially update this instance with a new value
+   * that would fall in the nth-highest values.  If n
+   * values have already been encountered but this new
+   * value should fall in the nth-highest values, it 
+   * will replace one of the values that have already been
+   * encountered.  
+   * 
+   * <p>An update operates in constant time in the best case
+   * and logarthmic time (relative to n) in the worst case.
+   */
+  public void update(final T newValue) {
+    int newValueIndex = INDEX_UNDEFINED; 
+
+    if (this.heapSize == 0) {
+      newValueIndex = 0;
+      this.maxHeap[newValueIndex] = newValue;
+      this.heapSize = 1;
+    } else if (this.heapSize < maxHeap.length) {
+      newValueIndex = heapSize;
+      this.maxHeap[newValueIndex] = newValue;
+      this.heapSize++;
+      
+      heapifyUp(newValueIndex); 
+    } else if (isGreaterThanNthValue(newValue)) { // heap full
+      
+       
+    } 
+
+    // if new value put in the heap (added or replaced)
+    if (newValueIndex != INDEX_UNDEFINED) {
+      final T currentMinValue = getMinValue();
+      
+      if (currentMinValue.compareTo(newValue) > 0) {
+        this.minValueIndex = newValueIndex;
+      }
+    }
+  }
+  
+  private boolean isHeapFull() {
+    return this.maxHeap.length == this.heapSize;
+  }
+
+  @SuppressWarnings("unchecked")
+  private T getMinValue() {
+    return (T)this.maxHeap[this.minValueIndex];
+  }
+
+  private void heapifyUp(final int childIndex) {
+  
+  }
+
+  private boolean isGreaterThanNthValue(T obj) {
+    return false; 
+  }
 }
--- src/test/java/com/github/vermasque/NthHighestValueTest.java
+++ src/test/java/com/github/vermasque/NthHighestValueTest.java
@@ -1,12 +1,26 @@
 package com.github.vermasque;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import java.util.Optional;
+import java.util.stream.Stream;
+
 public class NthHighestValueTest {
+
   @Test
   public void sequenceLargerThanN() {
-    assertTrue(true);
+    final var thirdHighestValueTracker = new NthHighestValue<Integer>(3);
+
+    Stream
+      .of(2, 1, 7, 8, 4)
+      .forEach(thirdHighestValueTracker::update);
+
+    final Optional<Integer> thirdHighestValue = thirdHighestValueTracker.get(); 
+
+    assertTrue(thirdHighestValue.isPresent());
+    assertEquals(4, thirdHighestValue.get().intValue());
   }
 }
